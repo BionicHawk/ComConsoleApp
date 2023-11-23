1bb55b3e6cf13c06ec821957365c4c72e34400d2
BionicHawk <angel.manzo.rosas@gmail.com>
2023-11-23 01:53:12 -0600

    Agregar archivos de proyecto.

---
+++ ComConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryC_;
+
+namespace ComConsoleApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var objCOMPlus = new COMPlusServices();
+			Console.WriteLine(objCOMPlus.DoTransaction());
+			Console.ReadLine();
+		}
+	}
+}
